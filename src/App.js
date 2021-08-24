2367729b72b65d49839a0cbbddcfad6437659723
Justin Hamerly <suvimountain@icloud.com>
2021-08-23 19:16:31 -0700

    lab06 finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 import axios from 'axios';
 import Accordion from 'react-bootstrap/Accordion';
+import Alert from 'react-bootstrap/Alert';
 
 let cityKey = process.env.REACT_APP_CITY_KEY;
 
@@ -15,20 +16,21 @@ class App extends Component {
       lat: '',
       lon: '',
       name: '',
+      error: '',
     }
   }
   getLocation = async () => {
     const API = `https://us1.locationiq.com/v1/search.php?key=${cityKey}&q=${this.state.searchQuery}&format=json`;
     try{
       const response = await axios.get(API);
-      console.log(response.data);
+      // console.log(response.data);
       this.setState({location: response.data[0]});
       this.setState({lat: response.data[0].lat});
       this.setState({lon: response.data[0].lon});
       this.setState({name: response.data[0].display_name});
       this.setState({image: `https://maps.locationiq.com/v3/staticmap?key=${cityKey}&center=${this.state.lat},${this.state.lon}&zoom=12`});
     } catch(err) {
-      console.log(err.message);
+      this.setState({error: `${err}`});
     }
   }
   render(){
@@ -37,16 +39,23 @@ class App extends Component {
         <h1>City Explorer!</h1>
         <input onChange={(e) => this.setState({searchQuery: e.target.value})} value={this.state.searchQuery} placeholder="search for a city"></input>
         <button onClick={this.getLocation}>Explore!</button>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header><h2>Location: {this.state.location.display_name}</h2></Accordion.Header>
-            <Accordion.Body>
-              <h3>Latitude: {this.state.lat}</h3>
-              <h3>Longitude: {this.state.lon}</h3>
-              <img src={this.state.image} alt={this.state.name} />
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        {this.state.error ?
+          <Alert>
+            {this.state.error}: Not a valid location
+          </Alert>
+        
+        : this.state.name &&
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header><h2>Location: {this.state.location.display_name}</h2></Accordion.Header>
+              <Accordion.Body>
+                <h3>Latitude: {this.state.lat}</h3>
+                <h3>Longitude: {this.state.lon}</h3>
+                <img src={this.state.image} alt={this.state.name} />
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        }
       </>
     )
   }
