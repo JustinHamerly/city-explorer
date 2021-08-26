c72e7c79ff55f37d78100e9f18cef6ea8a934906
Justin Hamerly <suvimountain@icloud.com>
2021-08-26 15:07:38 -0700

    heroku hook up and finishing lab 08

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Component } from 'react';
 import axios from 'axios';
 import Accordion from 'react-bootstrap/Accordion';
 import Alert from 'react-bootstrap/Alert';
-import Weather from './Weather.js'
+import Weather from './Weather'
+import Movies from './Movies'
 
 
 let cityKey = process.env.REACT_APP_CITY_KEY;
+let server = process.env.REACT_APP_SERVER || 'https://localhost:3001';
 
 class App extends Component {
   constructor(props){
@@ -19,6 +21,7 @@ class App extends Component {
       lon: 0,
       error: '',
       weather: undefined,
+      movies: undefined,
     }
   }
 
@@ -26,12 +29,16 @@ class App extends Component {
     const API = `https://us1.locationiq.com/v1/search.php?key=${cityKey}&q=${this.state.searchQuery}&format=json`;
     try{
       const response = await axios.get(API);
-      this.setState({location: response.data[0]});
-      this.setState({lat: parseFloat(response.data[0].lat)})
-      this.setState({lon: parseFloat(response.data[0].lon)})
-      this.setState({image: `https://maps.locationiq.com/v3/staticmap?key=${cityKey}&center=${this.state.lat},${this.state.lon}&zoom=13`});
-      this.setState({weather: undefined});
+      this.setState({
+        location: response.data[0],
+        lat: parseFloat(response.data[0].lat),
+        lon: parseFloat(response.data[0].lon),
+        image: `https://maps.locationiq.com/v3/staticmap?key=${cityKey}&center=${this.state.lat},${this.state.lon}&zoom=13`,
+        weather: undefined,
+        movies: undefined,
+      });
       this.getWeather(this.state.searchQuery, this.state.lat, this.state.lon);
+      this.getMovies(this.state.searchQuery);
     } catch (err) {
       this.setState({error: `${err}`});
     }
@@ -39,8 +46,18 @@ class App extends Component {
 
   getWeather = async (searchQuery, lat, lon) => {
     try{
-      const response = await axios.get(`http://localhost:3001/weather?searchQuery=${searchQuery}&lat=${lat}&lon=${lon}`);
+      const response = await axios.get(`${server}/weather?searchQuery=${searchQuery}&lat=${lat}&lon=${lon}`);
       this.setState({weather: response.data});
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getMovies = async (searchQuery) => {
+    try{
+      const response = await axios.get(`${server}/movies?searchQuery=${searchQuery}`)
+      this.setState({movies: response.data});
+      console.log(this.state.movies);
     } catch (error) {
       console.log(error);
     }
@@ -73,6 +90,9 @@ class App extends Component {
             {this.state.weather &&
               <Weather weatherData={this.state.weather} />
             } 
+            {this.state.movies &&
+              <Movies movieData={this.state.movies} />
+            }
           </>
         }
       </>
---
+++ src/Movies.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from 'react-bootstrap/Accordion';
+
+class Movies extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movieData: this.props.movieData,
+    }
+  }
+  render() {
+    let movieArray = this.props.movieData;
+    return(
+      <>
+        {movieArray.map((movie, idx) => {
+          return(
+            <Accordion defaultActiveKey={idx}>
+            <Accordion.Item eventKey={movie.title}>
+              <Accordion.Header><h2>Movie: {idx+1}</h2></Accordion.Header>
+              <Accordion.Body>
+                <h3>Title: {movie.title}</h3>
+                <h4>Description: {movie.description}</h4>
+                <h4>Average Vote: {movie.avgVote}</h4>
+                <h4>Total Votes: {movie.totalVote}</h4>
+                <h4>Popularity: {movie.popularity}</h4>
+                <h4>Release Date: {movie.release_date}</h4>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+          ) 
+        })};
+      </>
+    );
+  }
+}
+
+export default Movies;
